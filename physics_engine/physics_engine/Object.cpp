b66e43e7b7a82a1bcff18229e45b4165867ba191
Spencer McLaughlin <spencer.pg@gmail.com>
2015-11-19 21:07:17 +0000

    - Added some basic physical properties to object class.

--- physics_engine/physics_engine/Object.cpp
+++ physics_engine/physics_engine/Object.cpp
@@ -2,11 +2,17 @@
 
 
 
-Object::Object()
+
+Object::Object(double mass)
+    :mMass(mass)
 {
 }
 
-
 Object::~Object()
 {
+}
+
+void Object::setMass(double mass)
+{
+    mMass = mass;
 }
--- physics_engine/physics_engine/Object.h
+++ physics_engine/physics_engine/Object.h
@@ -1,10 +1,23 @@
 #pragma once
+#define GLM_FORCE_CXX14
+#include <glm/vec3.hpp>
+
 class Object
 {
 public:
-    Object();
+    Object(double mass = 0.0);
     virtual ~Object() = 0;
 
     virtual void print() = 0;
+
+    // Accessors/Mutators
+    void setMass(double mass);
+
+private:
+    double      mMass;
+    glm::vec3   mPos;
+    glm::vec3   mRot;
+    glm::vec3   mForce;
+    glm::vec3   mTorque;
 };
 
