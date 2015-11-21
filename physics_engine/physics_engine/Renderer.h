8e0c3bf451f8999dc193437dadd0e3a3903a3c93
Spencer McLaughlin <spencer.pg@gmail.com>
2015-11-21 00:37:03 +0000

    - Skeleton implementation for renderer. It just displays a triangle for now. Need to implement boxes/spheres next. Followed by a simple FPS camera. - Added GLFW and GLEW include paths to project - Updated documentation to reflect new reqs.

--- physics_engine/physics_engine/Renderer.h
+++ physics_engine/physics_engine/Renderer.h
@@ -3,13 +3,20 @@
 #include <vector>
 #include "Object.h"
 
+struct GLFWwindow;
+
 class Renderer
 {
 public:
 	Renderer(std::vector<Object*>& objList);
 	~Renderer();
 
+    bool init();
+    void render();
+    void close();
+
 private:
     std::vector<Object*>&   mObjList;
+    GLFWwindow*             mWindow;
 };
 
