e4ef434f31d93585dbc1b556df62eb8e438d00b3
Spencer McLaughlin <spencer.pg@gmail.com>
2016-04-23 23:49:52 +0100

    added basic impulse collision response for sphere sphere collisions.

--- physics_engine/physics_engine/Physics.cpp
+++ physics_engine/physics_engine/Physics.cpp
@@ -31,13 +31,21 @@ bool Physics::update()
     }
 
 	// narrowphase
-    for(auto colPair : potentialCollisions)
+    for(const auto& colPair : potentialCollisions)
     {
         CollisionData colData;
-        if(Collision::areObjectsColliding(colPair.first->phys(), colPair.second->phys(), &colData))
+        PhysicsObject& po1 = colPair.first->phys();
+        PhysicsObject& po2 = colPair.second->phys();
+        if(Collision::areObjectsColliding(po1, po2, &colData))
         {
-            colPair.first->phys().setVel(glm::dvec3(0.0));
-            colPair.second->phys().setVel(glm::dvec3(0.0));
+            //colPair.first->phys().setVel(glm::dvec3(0.0));
+            //colPair.second->phys().setVel(glm::dvec3(0.0));
+            auto CoR = 0.7;
+            auto v_ab = po1.vel() - po2.vel();
+            auto J = -(1 + CoR) * (glm::dot(v_ab, colData.normal)) / (po1.invMass() + po2.invMass());
+
+            po1.setVel(po1.vel() + J * po1.invMass() * colData.normal);
+            po2.setVel(po2.vel() - J * po2.invMass() * colData.normal);
         }
     }
 
